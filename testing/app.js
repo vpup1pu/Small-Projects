95402fd1d0311eacb6496703a4d94a873c9f633c
Ainhoa Vargas <ainhoabelenvargas@gmail.com>
2023-10-09 11:54:27 -0300

    odin proyect finished

--- testing/app.js
+++ testing/app.js
@@ -1,50 +1,24 @@
-const container = document.querySelector('#container');
+const unorderL = document.querySelector("ul");
+const box = document.querySelector("input");
+const btn = document.querySelector("button");
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+btn.addEventListener('click', () =>{
+    const item = box.value;
+    box.value = '';
 
-container.appendChild(content);
+    const list = document.createElement("li");
+    const span = document.createElement("span");
+    const deleteBtn = document.createElement("button");
+    
+    list.appendChild(span);
+    span.textContent = item;
+    list.appendChild(deleteBtn);
+    deleteBtn.textContent = "Delete";
+    unorderL.appendChild(list);
 
-const para1 = document.createElement('p');
-para1.textContent = "Hey I'm red!";
-para1.setAttribute('style', 'color: red');
-container.appendChild(para1);
+    deleteBtn.addEventListener('click', ()=>{
+        unorderL.removeChild(list);
+    })
 
-const head3 = document.createElement('h3');
-head3.textContent = "Hey I'm blue!";
-head3.setAttribute('style', 'color: blue');
-container.appendChild(head3);
-
-const box = document.createElement('div');
-const head1 = document.createElement('h1');
-head1.textContent = "I'm in a div";
-const para2 = document.createElement('p');
-para2.textContent = "ME TOO!";
-box.appendChild(head1);
-box.appendChild(para2);
-box.classList.toggle('box');
-box.setAttribute('style', 'background-color: pink; border: 5px solid black');
-container.appendChild(box);
-
-// const btn = document.querySelector('#btn');
-// btn.onclick = () => alert("Hello World");
-
-// const btn = document.querySelector('#btn');
-// btn.addEventListener('click', () => {
-// 		alert("Hello World");
-// 	});
-
-function alertFunction() {
-    alert("YAY! YOU DID IT!");
-}
-
-// METHOD 2
-btn.onclick = alertFunction;
-
-// METHOD 3
-btn.addEventListener('click', alertFunction);
-
-btn.addEventListener('click', function (e) {
-    e.target.style.background = 'blue';
-  });
+    box.focus();
+})
