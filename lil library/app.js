557d529100d1beabc378c545ffbcf311a42d0ff0
Ainhoa <ainhoabelenvargas@gmail.com>
2024-03-01 14:11:25 -0300

    library improves

--- lil library/app.js
+++ lil library/app.js
@@ -1,4 +1,7 @@
-const myLibrary = [];
+const myLibrary = [
+  // new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet'),
+  // new Book('Odisea', 'Homero', 480, 'read')
+];
 const titleInput = document.getElementById("bookTitle");
 const authorInput = document.getElementById("bookAuthor");
 const pagesInput = document.getElementById("bookPages");
@@ -15,6 +18,9 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
 }
 
 function isInputValid(input) {
@@ -30,6 +36,7 @@ function addBookToLibrary(title, author, pages, read) {
     if (!bookExists(title, author)) {
       let newBook = new Book(title, author, pages, read);
       myLibrary.push(newBook);
+      displayLibrary();
       error.textContent = "";
       success.textContent = "Successfully uploaded!";
       success.style.color = "green";
@@ -53,10 +60,33 @@ showButton.addEventListener("click", () => {
 });
 
 closeButton.addEventListener("click", () => {
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.value = "";
   dialog.close();
   error.textContent = "";
+  success.textContent = "";
 });
 
 addButton.addEventListener("click", () => {
   addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);;
 });
+
+function displayLibrary() {
+  const display = document.querySelector(".libContainer");
+  display.innerHTML = "";
+
+  myLibrary.forEach(book => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+
+    const info = document.createElement("div");
+    info.textContent = book.info();
+    bookCard.appendChild(info);
+
+    display.appendChild(bookCard);
+  });
+}
+
+displayLibrary();
